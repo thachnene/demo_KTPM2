894ca78f2c18ed5b4b2285478e9a4f0b94846608
thachnene <1951012124thach@ou.edu.vn>
2023-10-23 15:43:39 +0700

    15 cap nhat lai file thongbao.cpp vao nhanh-15

--- thongbao.cpp
+++ thongbao.cpp
@@ -6,5 +6,6 @@ int main() {
 	cout << "Hello!\n";
 	cout << "Tao nhanh-15 va thay doi file thongbao.cpp" << endl;
 	cout << "Merge bang cach thuc hien tren local" << endl;
+	cout << "Thay doi noi dung file thongbao.cpp va cap nhat vao nhanh-15" << endl;
 	return 0;
 }
